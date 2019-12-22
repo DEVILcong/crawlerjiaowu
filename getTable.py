735cc6a06b52a3b4f810132f14b2da67a363dbc7
DEVILcong <liangyuecong@sina.com>
2019-12-22 22:32:46 +0800

    small changes 20191222

--- getTable.py
+++ getTable.py
@@ -9,5 +9,16 @@ def getTable(fileLoc):
     with open(fileLoc) as fileT:
         content = fileT.read()
     
-    table = [[''for i in range(8)] for j in range(11)]
+    tableO = [[''for i in range(8)] for j in range(11)]
+    tableO[0][0] = '星期一'
+    tableO[0][1] = '星期二'
+    tableO[0][2] = '星期三'
+    tableO[0][3] = '星期四'
+    tableO[0][4] = '星期五'
+    tableO[0][5] = '星期六'
+    tableO[0][6] = '星期日'
     soup = bs(content, 'lxml')
+    table = soup.table
+    tr = table.find_all(name = 'tr')
+
+
